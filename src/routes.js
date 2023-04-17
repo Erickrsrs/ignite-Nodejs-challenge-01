3af5351c68c7b6cc746d4f28d3b3a460f872c54d
Erick Rocha <erick2003rocha@gmail.com>
2023-04-17 20:38:50 -0300

    cria funções para lidar com params e query. cria função para lidar com o body da req

--- src/routes.js
+++ src/routes.js
@@ -1,40 +1,40 @@
 import { Database } from './database/database.js';
+import { buildRoutePath } from './utils/build-route-path.js';
 
 const database = new Database();
-database.persist();
 
 export const routes = [
   {
     method: 'GET',
-    path: '/tasks',
+    path: buildRoutePath('/tasks'),
     handler: (req, res) => {
       res.end('get');
     },
   },
   {
     method: 'POST',
-    path: '/tasks',
+    path: buildRoutePath('/tasks'),
     handler: (req, res) => {
       res.end('post');
     },
   },
   {
     method: 'PUT',
-    path: '/tasks',
+    path: buildRoutePath('/tasks/:id'),
     handler: (req, res) => {
       res.end('put');
     },
   },
   {
     method: 'DELETE',
-    path: '/tasks',
+    path: buildRoutePath('/tasks/:id'),
     handler: (req, res) => {
       res.end('delete');
     },
   },
   {
     method: 'PATCH',
-    path: '/tasks',
+    path: buildRoutePath('/tasks/:id/complete'),
     handler: (req, res) => {
       res.end('vpatch');
     },
